14456edab15f245714cc42ec3f32f4cd67b7b945
JoaoLucasFSantos <joaolucasferreiradossantos73@gmail.com>
2025-11-19 01:25:46 -0300

    Att

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  // --- CORREÇÃO PARA GITHUB PAGES ---
-  // Substitua 'nome-do-seu-repositorio' pelo nome exato que está na URL do seu GitHub.
-  // Exemplo: Se a URL é github.com/joao/projeto-requalifica, coloque '/projeto-requalifica/'
+
   base: "/reQualifica-Gs2/", 
 })
